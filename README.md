15785a6736f7d64bd55e24bcea9f53997043f798
Flatnoise <snownoise@gmail.com>
2019-04-21 12:10:19 +0300

    Added calculation for colony Orglevel and Stability
    Changes to be committed:
    	modified:   README.md
    	modified:   slcm_main.py

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # SLCM
 Sublight Colonisation Modeller
 
+See wiki for more details
 
-
-# Ranges and time definitions.
-On ingame time tick is 1 megasecond (1 ms = 1000000 seconds) which is about 11.57 days
-One range unit is 1 light megasecond (1 lms) which is about 0.0317 light years
-So, for example, Proxima Centauri is 4.244 light years from Sun, which is 133.88 lms
--- slcm_main.py
+++ slcm_main.py
@@ -1,5 +1,6 @@
 import random
 import math
+import matplotlib.pyplot as plt
 
 def calc_range(x1, y1, x2, y2):
     """
@@ -43,11 +44,13 @@ class Planet():
         return text
 
 class Colony():
-    def __init__(self, planet_id, population, techlevel, stability, name=""):
+    def __init__(self, planet_id, population, techlevel, orglevel, stability, name=""):
         self.planet_id = planet_id
         self.name = name
         self.population = population
         self.techlevel = techlevel
+        self.orglevel = orglevel
+        self.orglevel_progress = 0.0
         self.stability = stability
 
     def __str__(self):
@@ -55,8 +58,49 @@ class Colony():
             f'Name:\t{self.name}\n'
             f'Population:\t{self.population}\n'     # In thousands of people
             f'Techlevel:\t{self.techlevel}\n'       # Techlevel. From 1 to 20
-            f'Stability:\t{self.stability}')        # Internal stability of society (very basic approach). From 1 to 100
+            f'Orglevel:\t{self.orglevel}\n'         # Orglevel. From 1 to 20
+            f'Stability:\t{self.stability}')        # Internal stability of society (very basic approach).
         return text
+
+    def calculate_stability(self, turn):
+        # Calculate random events each 10 turns
+        if (turn % 10 == 0):
+            r = int(random.gauss(0.5, 10 / self.orglevel) * 1000 + 4500)
+            if r > 1 and r < 1000:
+                # Epic failure event
+                stab_change = int(self.stability * -0.85)
+
+            else:
+                stab_change = int((r - 5000) * 0.002 * self.techlevel)
+
+
+            self.stability += stab_change
+
+        if self.stability > 10000: self.stability = 10000
+        if self.stability < 1: self.stability = 1
+
+    def calculate_orglevel(self, turn, planet_habitability):
+
+        if turn % 10 == 0:
+            org_change = (random.random() - 0.5) * ((99 / (self.population / 1000000)) + 1)
+        else:
+            org_change = (self.stability ** (0.25) - 8) * (10 / planet_habitability)
+
+        self.orglevel_progress += org_change
+
+        # Levelup code
+        if self.orglevel_progress > (self.orglevel**2 * 10):
+            self.orglevel += 1
+            self.orglevel_progress = 0
+
+        # Leveldown code
+        elif self.orglevel_progress < (-1 * ((20 - self.orglevel)**1.6 * 10 + 300)):
+            self.orglevel -= 1
+            self.orglevel_progress = 0
+
+        # Verifing ranges
+        if self.orglevel > 20: self.orglevel = 20
+        if self.orglevel < 1: self.orglevel = 1
 
 
 # Settings
@@ -135,12 +179,44 @@ def initial_system_generation():
 # Init
 settings = Settings()
 stars, terra_id = initial_system_generation()
+max_turns = 1000001
+
+# Lists for plotting graps
+plot_turn = []
+plot_orglevel = []
+plot_stability = []
+#plot_r = []
 
 colonies = []
-temp_colony = Colony(terra_id, 1000000, 10, 80, "Terra")
-print(temp_colony)
+temp_colony = Colony(terra_id, 1000000, 10, 10, 7000, "Terra")
+# print(temp_colony)
 
 
 # print(settings, '\n\n\n')
 # print(stars[0].planets[0], '\n\n')
 # print(stars[0])
+
+for turn in range(1, max_turns):
+    # print(turn)
+    plot_turn.append(turn)
+    plot_stability.append(temp_colony.stability)
+    plot_orglevel.append(temp_colony.orglevel * 500)
+
+    temp_colony.calculate_stability(turn)
+    temp_colony.calculate_orglevel(turn, 100)
+
+plt.plot(plot_turn, plot_stability)
+plt.plot(plot_turn, plot_orglevel)
+#plt.plot(plot_turn, plot_r)
+print("*****")
+print(f"min(stability)={min(plot_stability)}")
+print(f"max(stability)={max(plot_stability)}")
+# print(f"min(random)={min(plot_r)}")
+# print(f"max(random)={max(plot_r)}")
+
+# for i in range(1,20):
+#     print(i, (i**2 * 10),int(-1* ((20-i)**1.6 * 10 + 300)))
+
+plt.show()
+
+
